fd34eb271ace5fbfbf656956f96be63f63629bc4
Mona-kuebelbeck <mona.kuebelbeck@hfg.design>
2023-01-30 19:25:19 +0100

    Video rewind effect

--- source/js/main.js
+++ source/js/main.js
@@ -116,6 +116,7 @@ let balls;
 /** @type {number[]} */ let playerpositioncar = [];
 let isCarWindingUp = false;
 /** @type {Image} */ let gifRewind;
+/** @type {Image} */ let gifRewindOverlay;
 /** @type {Image} */ let imgElevator;
 /** @type {Image} */ let imgRocket;
 
